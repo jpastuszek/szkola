79e543b2c0f5e45bb511e90ec57e2d2aa264415d
Jakub Pastuszek <jpastuszek@gmail.com>
2012-04-19 19:42:07 +0200

    more form types; validation

--- strony/php/validation.js
+++ strony/php/validation.js
@@ -3,17 +3,32 @@ $(document).ready(function() {
 
 	var rules = {
 		required : {
-			check : function(value) {
-				if (value == "")
+			check : function(field) {
+				if (field.val() == "")
 					return false;
 				return true;
 			}
 		},
 		email : {
-			check : function(value) {
-				if (value.match(/.+@.+\..+/))
+			check : function(field) {
+				if (field.val().match(/.+@.+\..+/))
 					return true;
 				return false;
+			}
+		},
+		selected: {
+			check : function(field) {
+				if (field.val() == "") {
+					for (var element in field[0].elements) {
+						element = field[0].elements[element];
+						if (element.checked)
+							return true;
+					}
+					return false;
+				}
+				if (field.val() == "none")
+					return false;
+				return true;
 			}
 		}
 	}
@@ -34,7 +49,7 @@ $(document).ready(function() {
 						continue;
 					console.log(validator);
 
-					if (validator.check(field.val())) {
+					if (validator.check(field)) {
 						console.log("valid");
 						field.removeClass("error");
 					} else {
